1e30f2c9ac80fd3608f6e6507ba5703250423262
PeterSonne <papa@deep2chill.de>
2020-02-28 19:10:44 +0800

    Added Channel selection and DB sourcing

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,44 +2,42 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import "./styles/Sidebar.css";
 import { log } from "util";
+import Axios from "axios";
 
 class Sidebar extends Component {
   // Data
   state = {
     workspace: "Tortuga Coders",
-    channels: [
-      {
-        _id: "1",
-        name: "general",
-        active: true
-      },
-      {
-        _id: "2",
-        name: "announcements"
-      },
-      {
-        _id: "3",
-        name: "fun"
-      },
-      {
-        _id: "4",
-        name: "random"
-      },
-      {
-        _id: "5",
-        name: "coding"
-      }
-    ]
+    channels: []
   };
   // Lifecycle
-  componentWillMount() {}
+  componentWillMount() {
+    Axios.get(`${process.env.REACT_APP_API}/channels`, {
+      headers: { authorization: `Bearer ${localStorage.getItem("token")}` }
+    })
+      .then(docs => {
+        this.setState({
+          channels: docs.data.map((e, i) => {
+            e.active = i == 0 ? true : false;
+            return e;
+          })
+        });
+      })
+      .catch(err => console.log(err));
+  }
   // Methods
   logout = () => {
     localStorage.removeItem("token");
     // console.log(this.props);
     this.props.history.push("/");
   };
-  selectChannel = () => {};
+  selectChannel = id => {
+    let channels = this.state.channels.map(e => {
+      e.active = e._id == id;
+      return e;
+    });
+    this.setState({ channels });
+  };
   // Render
   render() {
     return (
